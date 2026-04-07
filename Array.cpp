d95e7da54756090022d2254708ad36e430030c57
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-04-07 15:09:17 +0700

    array

--- Array.cpp
+++ Array.cpp
@@ -13,5 +13,30 @@ int main(){
     dataku[2] = 600;
     cout << "data ke -3 : " << dataku[2] << endl;
 
-    
-}
+    cout << "data index ke - 0 : ";
+    cin  >> datamu[0];
+    cout << "data index ke - 1 : ";
+    cin  >> datamu[1];  
+    cout << "data index ke - 2 : ";
+    cin  >> datamu[2];
+    cout << "data index ke - 3 : ";
+    cin  >> datamu[3];
+    cout << "data index ke - 4 : ";
+    cin  >> datamu[4];
+
+    cout << "data ke -1 : " << datamu[0] << endl;
+    cout << "data ke -2 : " << datamu[1] << endl;    
+    cout << "data ke -3 : " << datamu[2] << endl;
+    cout << "data ke -4 : " << datamu[3] << endl;
+    cout << "data ke -5 : " << datamu[4] << endl;
+ 
+    cout << endl;
+
+    for (int i = 0; i < 5; i++){
+        cout << "data ke - " << i+1 << " : " << datamu[i] << endl;
+    }
+
+    for (int i = 0; i <= 4; i++) {
+    cout << "Data ke - " << i+1 << " : " << datamu[i] << endl;
+    }
+}   
